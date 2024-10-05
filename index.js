7298c7360aae40f496a51c5257c37120d51222b3
Ridwan <webridwan20@gmail.com>
2024-10-05 10:50:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// load function call below on the input (it is sniper function )
+const loadAllPhones = async(status, brandName) => {
+    console.log(brandName);
+    document.getElementById('spinner').style.display = "none"
+
+    // get the api form (async using)
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandName?brandName:'iphone'}`)
+    const data = await response.json();
+        console.log(data);
+        
+    
+    // show all btn condition (when you will click the show button)
+    if (status === true){
+        displayAllPhone(data.data)
+
+    } else{
+        displayAllPhone(data.data.slice(0,6))
+    }
+    // display all phones function
+}
+
+// display phone All phone function;
+const displayAllPhone = (phones) => {
+    const phoneContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
+        // console.log(phone);
+        
+        // new div create in
+        const div = document.createElement('div')
+        div.innerHTML = `
+        
+                <div class="card bg-base-100 w-96 mb-8 shadow-xl">
+                    <figure class="px-10 pt-10">
+                        <img
+                        src="${phone.image}"
+                        class="rounded-xl" />
+                    </figure>
+                        
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title">${phone.brand}</h2>
+                        <p>${phone.phone_name}</p>
+                        <div class="card-actions">
+                        <button class="bg-blue-600 px-4 py-1 rounded text-white">Show Details</button>
+                        </div>
+                    </div>
+                </div>
+        
+        `;
+        phoneContainer.append(div)
+    })
+    
+}
+
+// click show all  Btn function:
+const handleShowAll = () =>{
+   loadAllPhones(true)
+}
+
+
+// input button click: and with function create.
+const handlerSearch  = () => {
+    document.getElementById('spinner').style.display ="block";
+
+    // input value call with id.
+    const searchText = document.getElementById('search-box').value;
+
+    setTimeout (function () {
+        loadAllPhones(false, searchText)
+    }, 1000)
+};
+
+
+// call load phone
+loadAllPhones(false, 'iphone')
---
+++ note.js
@@ -0,0 +1,6 @@
+const obj = {
+    "brand": "Apple ",
+    "phone_name": "iPhone 13 mini",
+    "slug": "apple_iphone_13_mini-11104",
+    "image": "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg"
+};
